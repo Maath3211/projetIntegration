59ca62710e1cc62f329bb9d84e87d3d7bb993d65
Mathys Lessard <mathys.l.lessard@gmail.com>
2024-08-29 11:16:18 -0400

    changement message erreur

--- gestionFournisseur/resources/views/layouts/app.blade.php
+++ gestionFournisseur/resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="{{asset('gestion.css')}}">
+  <link rel="stylesheet" href="{{asset('css/gestion.css')}}">
   <title>@yield('title')</title>
 </head>
 <body>
-  @yield('contenu')
+  
   @if(isset($errors) && $errors->any())
     <div class="alert alert-danger">
       @foreach($errors->all() as $error)
@@ -23,6 +23,7 @@
         <p>{{ session('message') }}</p>
     </div>
   @endif
+    @yield('contenu')
   <footer>
     <p>&copy 2024-2025 Gestion, Inc.</p>
     <p>Simon Beaulieu &copy 2024</p>
