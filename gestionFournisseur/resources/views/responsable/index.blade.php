b4ad392afc2662517c929b673bc9c3de1f3932aa
Caporal6 <xavier.2@hotmail.ca>
2024-09-03 12:34:29 -0400

    Controller + page responsable creer

---
+++ gestionFournisseur/resources/views/responsable/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+@section('title',"Page d'accueil")
+<header>
+<div>
+    <a href="/"><h5 class="compagny">VILLE3R</h5></a>
+</div>
+</header>
+<div class="container bg-primary mb-3">
+    <div class="row">
+        <div class="col-12 ">
+            
+        <h1>Page d'administration</h1>
+            <!-- <div class="text-center py-5">
+
+                <div class="d-flex">
+                    <label>Courriel de l'appro.</label>
+                    <input type="text" class="form-control inputCourriel" placeholder="Courriel" name="emailAppro">
+                </div>
+
+                <div class="d-flex">
+                    <label>Délai avant la révision (mois)</label>
+                    <input type="number" class="form-control inputChiffre" placeholder="24" name="delai">
+                </div>
+
+                <div class="d-flex">
+                    <label>Taille maximale des fichiers joints (Mo)</label>
+                    <input type="number" class="form-control inputChiffre" placeholder="75" name="maxSize">
+                </div>
+
+                <div class="d-flex">
+                    <label>Courriel des finances</label>
+                    <input type="text" class="form-control inputCourriel" placeholder="Courriel" name="emailFinance">
+                </div>
+            </div> -->
+        </div>
+
+        <div class="col-12 bg-danger text-center p-5">
+            <h1>Tri</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container bg-primary">
+    <div class="row">
+        <div class="col-12 text-end">
+            
+        <h1 class="">Bouton exportation</h1>
+        </div>
+        <div class="col-12 bg-danger  p-5">
+            <h1>Tri</h1>
+        </div>
+    </div>
+</div>
--- gestionFournisseur/routes/web.php
+++ gestionFournisseur/routes/web.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GestionController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ResponsablesController;
 
 Route::GET('/',
 [GestionController::class,'index'])->name('gestion.index');
 
 Route::GET('/administration',
 [AdminController::class,'index'])->name('admin.index');
+
+Route::GET('/responsable',
+[ResponsablesController::class,'index'])->name('responsable.index');
