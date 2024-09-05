37ec1edd23d6fc36406c5d45fe1bc3dcc72ef1a1
Caporal6 <xavier.2@hotmail.ca>
2024-09-04 20:08:08 -0500

    FRONTEND UNSPSC fini manque juste le backend

--- gestionFournisseur/resources/views/fournisseur/UNSPSC.blade.php
+++ gestionFournisseur/resources/views/fournisseur/UNSPSC.blade.php
@@ -5,8 +5,104 @@
     <a href="/"><h5 class="compagny">LOGO-VILLE3R</h5></a>
 </div>
 </header>
-<div class="container bg-primary mb-3">
+<div class="container-fluid">
     <div class="row">
-        
+
+
+        <div class="col-md-2">
+
+        </div>
+
+
+    <div class="col-md-8 ">
+
+        <fieldset>
+          <legend>Produits et services offerts</legend>
+
+            <div class="row">
+                <div class="col-md-7">
+                    <h1>Services</h1>
+                </div>
+
+                <div class="col-md-3">
+                    <input type="text" class="form-control" maxlength="80">
+                </div>
+
+                <div class="col-md-2 mt-1">
+                    <Button>Logo</Button>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <h5 class="pl-5">S14 - Services Publics</h5>
+
+            <div class="col-md-1">
+                <input type="checkbox" class="mt-2">
+            </div>
+
+            <div class="col-md-4">
+                <p>17516152</p>
+            </div>
+
+            <div class="col-md-7">
+                <p>Chien et chat</p>
+            </div>
+                
+
+
+
+
+
+            <div class="row">
+                <h5 class="pl-5">Détails et spécifications</h5>
+
+
+                <div class="col-md-1">
+                    
+                </div>
+
+                <div class="col-md-10">
+                    <textarea name="Details" id="detail" class="form-control" maxlength="500"></textarea>
+                </div>
+
+                <div class="col-md-1">
+                    
+                </div>
+            </div>
+
+            <div class="row mt-4">
+
+                <div class="col-md-3">
+                    <button class="form-control">Précédent</button>
+                </div>
+
+                <div class="col-md-6">
+                </div>
+
+                <div class="col-md-3">
+                <button class="form-control">Suivant</button>
+                    
+                </div>
+            </div>
+
+        </fieldset>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <div class="col-md-2">
+            
+    </div>
+
+
     </div>
 </div>
